ef764ac36eb4437e84b6b44a82f3c94f5a92dd79
yosh dasturchi <javohirnabiyev5@gmail.com>
2022-03-09 06:34:40 -0500

    salom

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-tg_bot_token="5238766854:AAEhSwz000KaMk-bhf950O0ncsHpesGXQJ4"
-open_weather_token='d539db765863110016b27d8e1998f082'
+import os
+tg_bot_token=str(os.environ.get('tg_bot_token'))
+open_weather_token=str(os.environ.get('open_weather_token'))
+# tg_bot_token="5238766854:AAEhSwz000KaMk-bhf950O0ncsHpesGXQJ4"
+# open_weather_token='d539db765863110016b27d8e1998f082'
